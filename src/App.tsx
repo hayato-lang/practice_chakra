a5268bae0ad204b523904c41c5377fa4e345d650
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-08 23:07:22 +0900

    globalなスタイルを設定

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import {
   ChakraProvider,
   Button
 } from "@chakra-ui/react"
+import theme from "./theme/theme"
 
 
 export const App = () => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <Button colorScheme="teal">ハゲタコ</Button>
+    <p>aaaaa</p>
   </ChakraProvider>
 )
